0bcaa84a6c867769534361c7ecb146bdde9355b3
simoxvals <valenciasimon99@gmail.com>
2024-09-14 13:14:26 -0500

    Frecuencia, comparación de señales main.cpp
    Debo copiar y pegar el código completo directamente de tinkercad  por obvias razones,

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Adafruit_LiquidCrystal.h>
+
 Adafruit_LiquidCrystal lcd_1(0);
 
 int pinGenerador = 0;
@@ -7,13 +8,16 @@ int Estadoboton2 = 0;
 int numLecturas = 500;
 int* lecturas = nullptr;
 bool adquiriendoDatos = false;
-int tiempo_minimo = 100;
-float maximo=0;
+float maximo = 0;
 float* pFrecuencia = nullptr;
 float* pAmplitud = nullptr;
 float start = 0;
-float end  = 0;
+float end = 0;
 float N = 0;
+bool sCuadrada = false;
+bool sTriangular = false;
+bool cambioBrusco = false;
+bool esAnaloga = false;
 
 void liberarMemoria() {
     delete pFrecuencia;
@@ -23,7 +27,6 @@ void liberarMemoria() {
     pAmplitud = nullptr;
     lecturas = nullptr;
 }
-
 
 void asignarMemoria() {
     pFrecuencia = new float;
@@ -44,54 +47,106 @@ void setup() {
 void loop() {
     Estadoboton1 = digitalRead(2);
     Estadoboton2 = digitalRead(3);
-    
+
     if (Estadoboton1 == HIGH && !adquiriendoDatos) {
+        N = 0; 
+        maximo = 0;
         adquiriendoDatos = true;
+        sCuadrada = false;
+        sTriangular = false;
+        cambioBrusco = false;
+        esAnaloga = false;
         Serial.println("Iniciando adquisicion de datos...");
         start = millis();
     }
-    
+
     if (adquiriendoDatos) {
-        
-        for (int i = 0; i < numLecturas; i++) {
+        for (int i = 0; i < numLecturas; i+=20) {
             lecturas[i] = analogRead(pinGenerador);
             Serial.println(lecturas[i]);
             delay(10);
-            
-            
-            if (lecturas[i]>=maximo) {
-                maximo=lecturas[i];
-                if(lecturas[i]== maximo){
-                    N++;
+
+            if (lecturas[i] >= maximo) {
+                maximo = lecturas[i];
+            }
+
+            if (i >= 50 && abs(lecturas[i] - lecturas[i-20]) > 100) {
+                cambioBrusco = true;
+            }
+
+            if (cambioBrusco) {
+                
+                if (i >= 40 && lecturas[i] == lecturas[i-20] && lecturas[i-20] == lecturas[i-40]) {
+                    sCuadrada = true;
+                    maximo = lecturas[i];
+                }
+
+                if (i >= 40) {
+                    bool creciente = true;
+                    bool decreciente = true;
+                    for (int j = i-20; j >= i-40; j -= 20) {
+                        if (lecturas[j] < lecturas[j+20]) {
+                            decreciente = false;
+                        }
+                        if (lecturas[j] > lecturas[j+20]) {
+                            creciente = false;
+                        }
+                    }
+                    if (creciente && decreciente) {
+                        sTriangular = true;
+                    }
                 }
             }
-            
+
+            if (i >= 5 && lecturas[i] > 0 && lecturas[i-20] < 0) {
+                N++;
+            }
+
             if (digitalRead(3) == HIGH) {
                 Serial.println("Adquisicion de datos detenida por boton 2.");
                 adquiriendoDatos = false;
-                *pAmplitud = (maximo/100.0);
+                *pAmplitud = abs(maximo / 100.0);
                 end = millis() - start;
-                *pFrecuencia = (N / (end/1000));
+                *pFrecuencia = (N / (end / 1000.0));
+                
+                int parteEntera = (int)(*pFrecuencia);
+                float parteDecimal = (*pFrecuencia) - parteEntera;
+                if (parteDecimal > 0.35) {
+                    *pFrecuencia = parteEntera + 1;
+                } else {
+                    *pFrecuencia = parteEntera + parteDecimal;
+                }
+
+                if (!cambioBrusco) {
+                    esAnaloga = true; 
+                }
+ 
+                lcd_1.clear();
+                lcd_1.setCursor(0, 0);
+                if (sCuadrada) {
+                    lcd_1.print("Senal Cuadrada:");
+                } else if (sTriangular) {
+                    lcd_1.print("Senal Triangular:");
+                } else if (esAnaloga) {
+                    lcd_1.print("Senal Analogica:");
+                } else {
+                    lcd_1.print("Senal Desconocida:");
+                }
+                lcd_1.setCursor(0, 1);
+                lcd_1.print("Amplitud: ");
+                lcd_1.print(*pAmplitud);
+                lcd_1.print("V");
+                lcd_1.setCursor(0, 1);
+                lcd_1.print("Frecuencia: ");
+                lcd_1.print(*pFrecuencia);
+                lcd_1.print("Hz");
+
+                liberarMemoria();
+                asignarMemoria();
                 break;
             }
-            
-        }
-        if (!adquiriendoDatos) {
-            lcd_1.clear();
-            lcd_1.setCursor(0, 0);
-            lcd_1.print("Amp:");
-            lcd_1.print((*pAmplitud));
-            lcd_1.print("V");
-            lcd_1.setCursor(0, 1);
-            lcd_1.print("Fr: ");
-            lcd_1.print((*pFrecuencia ));
-            lcd_1.print("Hz");
-            liberarMemoria();
-            asignarMemoria();
         }
     }
 }
-
-
 
 
